c611a62b105c05199b9a80c73be64a78479b7ca6
SEGVeenstra <seg.veenstra@gmail.com>
2025-04-14 19:51:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# video_resource_go_router_todo_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/video_resource_go_router_todo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.video_resource_go_router_todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
